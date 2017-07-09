6faff1a9f8e8af337e544b18fe16f18a4f31dd94
Bermuda_UT <hoso1312@gmail.com>
2017-07-09 18:57:19 +1000

    framework to get started with server

--- bigbro-server/main.cc
+++ bigbro-server/main.cc
@@ -1,23 +1,23 @@
+/*=============================================================================
+#     FileName: main.cc
+#         Desc: driver for the server
+#       Author: Max Lee
+#        Email: hoso1312@gmail.com
+#     HomePage: mallocsizeof.me
+#      Version: 0.0.1
+#   LastChange: 2017-07-09 18:23:49
+=============================================================================*/
+#include "main.h"
 #include <iostream>
-#include <vector>
 
-using std::vector;
 using std::cout;
 using std::endl;
+
 //using namespace std;
 
-class Client {
-
-};
-
-class Server {
-    vector<Client> clients;
-    public:
-    int setup;
-};
-
-
 int main(int argc, char **argv) {
+    (void) argv;
+    (void) argc;
     cout << "hello world" << endl;
 }
 
---
+++ bigbro-server/main.h
@@ -0,0 +1,12 @@
+/*=============================================================================
+#     FileName: main.h
+#         Desc: 
+#       Author: Max Lee
+#        Email: hoso1312@gmail.com
+#     HomePage: mallocsizeof.me
+#      Version: 0.0.1
+#   LastChange: 2017-07-09 18:39:38
+=============================================================================*/
+#include "server.h"
+#include "client.h"
+
