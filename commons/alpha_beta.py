f6b1d4773f942277d95061dd547a7b4570a8a60f
jfong <jfong361@gmail.com>
2017-07-10 02:03:22 +1000

    correct next_move logic; increase pruning on root

--- commons/alpha_beta.py
+++ commons/alpha_beta.py
@@ -1,5 +1,3 @@
-import random
-
 class AlphaBeta(object):
     def __init__(self, max_depth):
         self.max_depth = max_depth
@@ -9,17 +7,14 @@ class AlphaBeta(object):
         returns a maximizing action from a given state by traversing a maximum of max_depth
         """
         possible_actions = state.get_actions()
+        max_depth_override = self.max_depth if not max_depth_override else max_depth_override
         if len(possible_actions) == 0:
             return None
-        best_move = possible_actions[int(random.random()*(len(possible_actions)-1))]
-        best_v = float('-inf')
-        max_depth_override = self.max_depth if not max_depth_override else max_depth_override
-        for move in state.get_actions():
-            v = self._min_move(state.apply_action_copy(move), float("-inf"), float("inf"), 1, max_depth_override)
-            if v > best_v:
-                best_move = move
-                best_v = v
-        return best_move
+        v = self._max_move(state, float("-inf"), float("inf"), 0, max_depth_override)
+        for move in possible_actions:
+            if state.apply_action_copy(move).evaluate() == v:
+                return move
+        raise Exception("Logic error, fatal")
 
     def _min_move(self, state, alpha, beta, depth, max_depth):
         if state.is_terminal() or depth >= max_depth:
