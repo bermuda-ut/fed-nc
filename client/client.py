3ff1799c537c2cd693b09f47845ba7c0c8523953
Matt F <matomatical@gmail.com>
2017-07-09 18:55:26 +1000

    generalised to a player interface

--- client/client.py
+++ client/client.py
@@ -1,19 +1,41 @@
-from noughtsandcrosses import board, piece
+from noughtsandcrosses import board, piece, InvalidMoveException
+from human import humanplayer
 
 def main():
 	b = board()
+	ps = {
+		piece.X: humanplayer(piece.X),
+		piece.O: humanplayer(piece.O)
+	}
+	print("player X: aiplayer")
+	print("player O: humanplayer")
 
+	move = None
+	error = None
 	p = piece.X
 
-	while True:
-		# get move from user
-		print(b)
-		move = input("your move: ")
+	print("begin!")
+	while b.playing():
+		print("\n{}".format(b))
 
-		# make move
-		b.move(move, p)
-		p = piece.other(p)
+		print("{}s turn".format(p))
+		ps[p].update(move)
+		move = ps[p].move()
 		
+		try:
+			b.move(move, p)
+		except InvalidMoveException as e:
+			error = e
+			break
+
+		p = piece.other(p)
+
+	if error:
+		print(error)
+	else:
+		print(b)
+		p[pi].update(move)
+		print("game over!")
 
 if __name__ == '__main__':
 	main()
--- client/noughtsandcrosses.py
+++ client/noughtsandcrosses.py
@@ -1,22 +1,34 @@
 class piece:
 	X, O = 'X', 'O'
+	blank = ' '
 	@staticmethod
 	def other(p):
 		return piece.O if p == piece.X else piece.X
 
 class board:
 	def __init__(self):
-		self.grid = [[str(i+j) for j in range(3)] for i in range(1, 9, 3)]
+		self.grid = [[piece.blank for j in range(3)] for i in range(3)]
 	def __str__(self):
 		rows = [" | ".join(row) for row in self.grid[::-1]]
 		board = " " + " \n---+---+---\n ".join(rows) + "\n"
 		return board
 	def move(self, move, p):
-		if len(move) != 1 or move not in "123456789":
+		if move < 1 or move > 9:
 			raise Exception("invalid move: {}".format(move))
 		
 		# find that part of the grid
-		m = int(move) - 1
+		m = move - 1
 		i, j = m // 3, m % 3
 		
+		# make sure it's legal
+		if self.grid[i][j] != piece.blank:
+			raise InvalidMoveException("invalid move: {} (space already occupied)".format(move))
+
+		# make the actual move
 		self.grid[i][j] = p
+		
+	def playing(self):
+		return True
+
+class InvalidMoveException(Exception):
+	"""Raised due to an invalid move"""
